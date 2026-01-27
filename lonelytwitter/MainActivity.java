c53e9d5adb74c69f13341c13333daf6d2c11d97e
faryal-inayat <faryalinayat34@gmail.com>
2026-01-27 12:55:17 +0500

    Add files via upload

---
+++ lonelytwitter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.lonelytwitter;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ImportantTweet t1 = new ImportantTweet("");
+        NormalTweet t2 = new NormalTweet("This is normal");
+        ArrayList<Tweet> tweetList = new ArrayList<>();
+        tweetList.add(t1);
+        tweetList.add(t2);
+        Mood m1 = new Happy();
+        Mood m2 = new Sad();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
