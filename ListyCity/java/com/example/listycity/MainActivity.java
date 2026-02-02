380bcbec17c9682aa0bf659c95eba2cc8e1583c7
faryalinayat <faryalinayat34@gmail.com>
2026-02-02 15:47:44 +0500

    Sprint 2

---
+++ ListyCity/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+
+    int selectedPosition = -1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        cityList = findViewById(R.id.city_list);
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedPosition = position;
+        });
+
+        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>( this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        Button addCity = findViewById(R.id.add_city);
+        EditText cityInput = findViewById(R.id.city_input);
+
+        addCity.setOnClickListener(v -> {
+            String newCity = cityInput.getText().toString().trim();
+
+            if (!newCity.isEmpty()) {
+                dataList.add(newCity);
+                cityAdapter.notifyDataSetChanged();
+                cityInput.setText("");
+            }
+        });
+
+        Button deleteCity = findViewById(R.id.delete_city);
+
+        deleteCity.setOnClickListener(v -> {
+            if (selectedPosition != -1) {
+                dataList.remove(selectedPosition);
+                cityAdapter.notifyDataSetChanged();
+                selectedPosition = -1;
+            }
+        });
+
+
+
+    }
+}
